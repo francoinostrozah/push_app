7074da35ed36bbdee44cb49e4ad255aaaa149fcd
Franco Inostroza <franco.inostrozah@gmail.com>
2023-11-18 12:53:18 -0300

    :sparkles: receive and send notification from firebase

--- android/app/src/main/kotlin/com/sazed/push_app/MainActivity.kt
+++ android/app/src/main/kotlin/com/sazed/push_app/MainActivity.kt
@@ -1,4 +1,4 @@
-package com.example.push_app
+package com.sazed.push_app
 
 import io.flutter.embedding.android.FlutterActivity
 
--- lib/presentation/blocs/notifications/notifications_bloc.dart
+++ lib/presentation/blocs/notifications/notifications_bloc.dart
@@ -1,15 +1,66 @@
 import 'package:equatable/equatable.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:push_app/firebase_options.dart';
 
 part 'notifications_event.dart';
 part 'notifications_state.dart';
+
+Future<void> firebaseMessagingBackgroundHandler(RemoteMessage message) async {
+  // If you're going to use other Firebase services in the background, such as Firestore,
+  // make sure you call `initializeApp` before using other Firebase services.
+  await Firebase.initializeApp();
+
+  print("Handling a background message: ${message.messageId}");
+}
 
 class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
   FirebaseMessaging messaging = FirebaseMessaging.instance;
 
   NotificationsBloc() : super(const NotificationsState()) {
-    // on<NotificationsEvent>((event, emit) {});
+    on<NotificationStatusChanged>(_notificationStatusChanged);
+
+    _initialStatusCheck();
+    _onForegroundMessage();
+  }
+
+  static Future<void> initializeFCM() async {
+    await Firebase.initializeApp(
+      options: DefaultFirebaseOptions.currentPlatform,
+    );
+  }
+
+  void _notificationStatusChanged(
+      NotificationStatusChanged event, Emitter<NotificationsState> emit) {
+    emit(state.copyWith(status: event.status));
+    _getFCMToken();
+  }
+
+  void _initialStatusCheck() async {
+    final settings = await messaging.getNotificationSettings();
+    add(NotificationStatusChanged(settings.authorizationStatus));
+  }
+
+  void _getFCMToken() async {
+    if (state.status != AuthorizationStatus.authorized) return;
+
+    final token = await messaging.getToken();
+
+    print(token);
+  }
+
+  void _handleRemoteMessage(RemoteMessage message) async {
+    print('Got a message whilst in the foreground!');
+    print('Message data: ${message.data}');
+
+    if (message.notification == null) return;
+
+    print('Message also contained a notification: ${message.notification}');
+  }
+
+  void _onForegroundMessage() {
+    FirebaseMessaging.onMessage.listen(_handleRemoteMessage);
   }
 
   void requestPermission() async {
@@ -23,6 +74,6 @@ class NotificationsBloc extends Bloc<NotificationsEvent, NotificationsState> {
       sound: true,
     );
 
-    settings.authorizationStatus;
+    add(NotificationStatusChanged(settings.authorizationStatus));
   }
 }
